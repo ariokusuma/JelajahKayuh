1979ffe3e67533834eaa4d46ecebca18abb52d84
ariokusuma <ariopuboyo@gmail.com>
2023-12-11 16:03:43 +0700

    test login

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    <a href="/login" type="submit" class="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login in</a>
+</body>
+</html>
