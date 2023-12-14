51e8847e8fab603226cbe6cb800111974c0367a6
raina717 <aprianiraina717@gmail.com>
2023-12-14 14:40:23 +0700

    payment done

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -5,6 +5,7 @@ use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 use App\Models\items;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use App\Models\orders;
 use App\Models\User;
@@ -24,13 +25,43 @@ class ItemsController extends Controller
 
         return view('pemesanan', [
             "pemesanan" => items::find($id),
-            
+
         ]);
+    }
+    public function postdetailpemesanan($id , Request $request){
+        $order = new orders();
+        $order->item_id = $id;
+        $order->user_id = Auth::user()->id;
+        $order->payment_evidence = null;
+        $order->status = 1;
+        $order->comments = 'No Telp Yang Bisa Di Hubugni' . $request->no_telp;
+        $order->start_date = $request->date;
+
+        $carbonDate = Carbon::parse($request->date);
+
+        if ($request->masa == '1'){
+            $order->end_date = $carbonDate->addDays(1);
+        }
+        if ($request->masa == '1/2'){
+            $order->end_date = $carbonDate;
+        }
+        if ($request->masa == '2'){
+            $order->end_date = $carbonDate->addDays(2);
+        }
+        if ($request->masa == '3'){
+            $order->end_date = $carbonDate->addDays(3);
+        }
+
+        $order->save();
+        return redirect('/');
+
+
+
     }
 
     public function getAllOrdersData(): View
     {
-        $AllOrdersData = orders::with('item', 'user')->get();
+        $AllOrdersData = orders::with('item', 'user')->where('user_id',Auth::id())->get();
 
         foreach ($AllOrdersData as $order) {
             $startDate = Carbon::parse($order->start_date);
@@ -63,4 +94,25 @@ class ItemsController extends Controller
 
         return view('profiluser', ['AllOrdersData' => $AllOrdersData]);
     }
+
+
+    public function bukti($id , Request $request){
+        // Menyimpan file ke dalam direktori yang diinginkan (misalnya, storage/app/public/bukti_transfer)
+        $file = $request->file('bukti_transfer');
+
+        $fileName = 'bukti_transfer_' . time() . '.' . $file->getClientOriginalExtension();
+        // Pindahkan file ke folder public
+        $file->move(public_path('bukti_transfer'), $fileName);
+
+        $data = orders::find($id);
+        $data->payment_evidence = $fileName;
+        $data->status = 2;
+        $data->update();
+
+
+
+        return redirect()->back();
+    }
+
+
 }
--- resources/views/pemesanan.blade.php
+++ resources/views/pemesanan.blade.php
@@ -9,8 +9,10 @@
         <h1 class="mb-4 text-5xl font-bold tracking-tight leading-none text-grey-900 ">Pengajuan Pinjaman</h1>
     </div>
 
-    
-    <form class="max-w-lg mx-auto">
+
+    <form method="post" action="{{route('pesan' , ['id'=>$pemesanan->id])}}" class="max-w-lg mx-auto" enctype="multipart/form-data">
+        @csrf
+        @method('post')
         <div class="mb-5">
             <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sepeda</label>
             <p class="text-md font-regular text-gray-900 dark:text-white">{{ $pemesanan->item_name }}</p>
@@ -27,23 +29,23 @@
 
         <div class="mb-5">
             <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Masa Sewa</label>
-            <select  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select  name="masa" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
-                <option>1 hari</option>
-                <option>1/2 hari</option>
-                <option>2 hari</option>
-                <option>3 hari</option>
+                <option value="1">1 hari</option>
+                <option value="1/2">1/2 hari</option>
+                <option value="2">2 hari</option>
+                <option value="3">3 hari</option>
               </select>
         </div>
-        
+
         <div class="mb-5">
             <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Sewa</label>
-            <input type="date" name="no_telp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <input type="date" name="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
         </div>
-       
+
         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lanjutkan Pemesanan</button>
       </form>
-</section> 
+</section>
 {{-- Form --}}
 
 
